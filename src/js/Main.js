642e6281ae3b35b1f8a91e349e2a5c385f16cf7e
Muhammed <muhammed.sari@student.odisee.be>
2017-04-27 16:45:01 +0200

    calling the render method from config

--- src/js/Main.js
+++ src/js/Main.js
@@ -2,6 +2,7 @@ import Configurator from './Configurator';
 
 {
 	const config = new Configurator();
+	config.render();
 	
 	// Event listeners
 	window.addEventListener('resize', config.resizeCanvas());
