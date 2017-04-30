57b72d35a6839181709c13886088bed78e832dc4
Muhammed <muhammed.sari@student.odisee.be>
2017-04-30 21:32:50 +0200

    indentation fix

--- src/js/data/Constants.js
+++ src/js/data/Constants.js
@@ -1,5 +1,5 @@
 export const CAMERA_LIMITS = Object.freeze({
-    LOWER_RADIUS: 50,
-    UPPER_RADIUS: 150,
-    UPPER_BETA  : 1.5,
+	LOWER_RADIUS: 50,
+	UPPER_RADIUS: 150,
+	UPPER_BETA  : 1.5,
 });
