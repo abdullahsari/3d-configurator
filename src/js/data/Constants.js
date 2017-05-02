90e1f7786b0f461e775b9eeca8e9e9a5f74a9a68
Muhammed <muhammed.sari@student.odisee.be>
2017-05-02 16:35:45 +0200

    added gravitational acceleration

--- src/js/data/Constants.js
+++ src/js/data/Constants.js
@@ -1,7 +1,12 @@
+// Camera
 export const CAMERA_LIMITS = Object.freeze({
-	LOWER_RADIUS: 50,
-	UPPER_RADIUS: 150,
-	UPPER_BETA  : 1.5,
+    LOWER_RADIUS: 50,
+    UPPER_RADIUS: 150,
+    UPPER_BETA  : 1.5,
 });
 
+// Physics
+export const GRAVITATIONAL_ACCELERATION = 9.81;
+
+// Directories
 export const TEXTURES_DIR = 'img/textures/';
