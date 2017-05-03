1cd45b21eaa117475b9d24938b9608c16fd9b3e6
Muhammed <muhammed.sari@student.odisee.be>
2017-05-03 14:09:38 +0200

    Scene is no longer being passed in constructor

--- src/js/entities/Skybox.js
+++ src/js/entities/Skybox.js
@@ -1,13 +1,14 @@
 import BABYLON from 'babylonjs';
+import Scene from '../core/Scene';
 import { TEXTURES_DIR } from '../data/Constants';
 
 const URL = `${TEXTURES_DIR}/skybox/skybox`;
 export default class Skybox {
-    constructor(scene) {
-        this.skybox = BABYLON.Mesh.CreateBox('skybox', 1000, scene);
-        const skyMat = new BABYLON.StandardMaterial('skybox', scene);
+    constructor() {
+        this.skybox = BABYLON.Mesh.CreateBox('skybox', 1000, Scene);
+        const skyMat = new BABYLON.StandardMaterial('skybox', Scene);
         skyMat.backFaceCulling = false;
-        skyMat.reflectionTexture = new BABYLON.CubeTexture(URL, scene);
+        skyMat.reflectionTexture = new BABYLON.CubeTexture(URL, Scene);
         skyMat.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
         skyMat.diffuseColor = new BABYLON.Color3(0, 0, 0);
         skyMat.specularColor = new BABYLON.Color3(0, 0, 0);
