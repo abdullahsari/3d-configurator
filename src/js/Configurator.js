215932d4b2ab1dde79a8063e452182e6473aec05
Muhammed <muhammed.sari@student.odisee.be>
2017-05-08 17:02:24 +0200

    added free camera and shadow generator

--- src/js/Configurator.js
+++ src/js/Configurator.js
@@ -25,14 +25,42 @@ export default class Configurator {
 
     init() {
 
+        // grab the canvas
+        const canvas = document.getElementById('renderContext');
+
         // define cameras
+        const arc = new BABYLON.ArcRotateCamera('arc', 1, 0.8, 10, new BABYLON.Vector3.Zero(), Scene),
+            free = new BABYLON.FreeCamera('free', new BABYLON.Vector3(0, 2, 50), Scene);
+
+        // rotation camera settings
+        arc.lowerRadiusLimit = CAMERA_LIMITS.LOWER_RADIUS;
+        arc.upperRadiusLimit = CAMERA_LIMITS.UPPER_RADIUS;
+        arc.upperBetaLimit = CAMERA_LIMITS.UPPER_BETA;
+        arc.attachControl(canvas);
+
+        // free camera settings
+        free.attachControl(canvas);
+        free.applyGravity = true;
+        free.checkCollisions = true;
+        free.keysUp.push(87); // W
+        free.keysDown.push(83); // S
+        free.keysLeft.push(65); // A
+        free.keysRight.push(68); // D
+        free.speed = 3;
+        free.inertia = 0.5;
+        free.angularSensibility = 1000;
+
         this.cameras = {
-            arc: new BABYLON.ArcRotateCamera('arc', 1, 0.8, 10, new BABYLON.Vector3.Zero(), Scene),
+            arc,
+            free
         };
-        this.cameras.arc.lowerRadiusLimit = CAMERA_LIMITS.LOWER_RADIUS;
-        this.cameras.arc.upperRadiusLimit = CAMERA_LIMITS.UPPER_RADIUS;
-        this.cameras.arc.upperBetaLimit = CAMERA_LIMITS.UPPER_BETA;
-        this.cameras.arc.attachControl(document.getElementById('renderContext'));
+
+        // deploy sun in the sky
+        this.sun = new Sun();
+
+        // generate shadows
+        this.shadowGenerator = new BABYLON.ShadowGenerator(4096, this.sun.light);
+        this.shadowGenerator.usePoissonSampling = true;
 
         // create skybox
         this.skybox = new Skybox();
@@ -42,9 +70,6 @@ export default class Configurator {
 
         // build wall
         this.wall = new SquareWall();
-
-        // deploy sun in the sky
-        this.sun = new Sun();
     }
 
     render() {
