10f700d308b49a1d8bfe420c053b0b8e4b72bb00
Muhammed <muhammed.sari@student.odisee.be>
2017-05-08 17:05:08 +0200

    light rays now come from the correct direction

--- src/js/entities/Sun.js
+++ src/js/entities/Sun.js
@@ -2,7 +2,7 @@ import Scene from '../core/Scene';
 import { DIRECTIONS, GROUND } from '../data/Constants';
 
 export default class Sun {
-    constructor() {
+    constructor(direction = DIRECTIONS.NORTH) {
 
         // create small sphere to visualize the origin of the light source
         const material = new BABYLON.StandardMaterial('sunMat', Scene);
@@ -14,44 +14,53 @@ export default class Sun {
 
         // create actual light source
         this.light = new BABYLON.DirectionalLight('sunlight', new BABYLON.Vector3(0, -1, 0), Scene);
+        this.light.intensity = 0.5;
 
         // move to default position
-        this.move(DIRECTIONS.NORTH);
+        this.move(direction);
     }
 
     move(direction) {
-        const radius = GROUND.SIZE * 0.75;
+        const radius = GROUND.SIZE * 0.75,
+            y = -1;
         let angle;
         switch (direction) {
             case DIRECTIONS.NORTH:
             default:
                 angle = 0;
+                this.light.direction = new BABYLON.Vector3(-1, y, 0);
                 break;
             case DIRECTIONS.NORTH_EAST:
-                angle = Utils.degreesToRadians(45);
+                angle = Utils.degreesToRadians(-45);
+                this.light.direction = new BABYLON.Vector3(-1, y, 1);
                 break;
             case DIRECTIONS.EAST:
-                angle = Utils.degreesToRadians(90);
+                angle = Utils.degreesToRadians(-90);
+                this.light.direction = new BABYLON.Vector3(0, y, 1);
                 break;
             case DIRECTIONS.SOUTH_EAST:
-                angle = Utils.degreesToRadians(135);
+                angle = Utils.degreesToRadians(-135);
+                this.light.direction = new BABYLON.Vector3(1, y, 1);
                 break;
             case DIRECTIONS.SOUTH:
-                angle = Utils.degreesToRadians(180);
+                angle = Utils.degreesToRadians(-180);
+                this.light.direction = new BABYLON.Vector3(1, y, 0);
                 break;
             case DIRECTIONS.SOUTH_WEST:
-                angle = Utils.degreesToRadians(225);
+                angle = Utils.degreesToRadians(-225);
+                this.light.direction = new BABYLON.Vector3(1, y, -1);
                 break;
             case DIRECTIONS.WEST:
-                angle = Utils.degreesToRadians(270);
+                angle = Utils.degreesToRadians(-270);
+                this.light.direction = new BABYLON.Vector3(0, y, -1);
                 break;
             case DIRECTIONS.NORTH_WEST:
-                angle = Utils.degreesToRadians(315);
+                angle = Utils.degreesToRadians(-315);
+                this.light.direction = new BABYLON.Vector3(-1, y, -1);
                 break;
         }
         const points = Utils.circleEquation(radius, angle);
         this.mesh.position.x = points[0];
         this.mesh.position.z = points[1];
-        this.light.direction = new BABYLON.Vector3(-points[0], -1, -points[1])
     }
 }
