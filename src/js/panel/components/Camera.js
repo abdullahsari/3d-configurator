ce70454276c412f1eb551a87add773568322a7ee
Muhammed Sari <muhammed.sari@student.odisee.be>
2017-05-13 22:31:26 +0200

    Created camera component

--- src/js/panel/components/Camera.js
+++ src/js/panel/components/Camera.js
@@ -1,3 +1,18 @@
-/**
- * Created by TURK on 13/05/2017.
- */
+import Config from '../../Configurator';
+
+export default class Camera {
+    constructor() {
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        const control = document.querySelector('#camera select');
+        control.addEventListener('change', () => {
+            this.changeCam(control.value);
+        });
+    }
+
+    changeCam(mode) {
+        Config.setCameraMode(mode);
+    }
+}
