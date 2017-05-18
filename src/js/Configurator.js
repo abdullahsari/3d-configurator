b2f9d406dfb577667e302568609b55e546b08118
Muhammed <muhammed.sari@student.odisee.be>
2017-05-18 15:33:50 +0200

    editor related functions

--- src/js/Configurator.js
+++ src/js/Configurator.js
@@ -8,9 +8,10 @@ import Sun from './entities/Sun';
 import Pole from './entities/Pole';
 import Wall from './entities/Wall';
 import Roof from './entities/Roof';
+import Material from './entities/Material';
 
 // Constants
-import { CAMERA_LIMITS, CAMERA_SETTINGS } from './data/constants';
+import { CAMERA_LIMITS, CAMERA_SETTINGS, STEP_AMOUNT } from './data/constants';
 
 /**
  * This class is the core of the application.
@@ -24,6 +25,7 @@ class Configurator {
         // Defaults
         this.entities = new Map();
         this.selected = null;
+        this.material = new Material();
 
         // Initialize 3D world
         this.initWorld();
@@ -38,7 +40,7 @@ class Configurator {
     initWorld() {
 
         // Define cameras
-        const arc = new BABYLON.ArcRotateCamera('arc', Math.PI, Math.PI / 2, CAMERA_LIMITS.UPPER_RADIUS, new BABYLON.Vector3.Zero(), Scene),
+        const arc = new BABYLON.ArcRotateCamera('arc', Math.PI, Math.PI / 2, CAMERA_LIMITS.UPPER_RADIUS, BABYLON.Vector3.Zero(), Scene),
             free = new BABYLON.FreeCamera('free', new BABYLON.Vector3(0, 2, 50), Scene);
 
         // Rotation-camera settings
@@ -79,17 +81,36 @@ class Configurator {
                 if (this.selected) {
                     this.entities.get(this.selected).mesh.material.wireframe = false;
                     this.selected = null;
+                    this.hideSections(true);
                 } else {
                     this.selected = result.pickedMesh.toString().split(' ')[1].slice(0, -1);
                     result.pickedMesh.material.wireframe = true;
+                    this.hideSections(false);
                 }
             } else {
                 if (this.selected) {
                     this.entities.get(this.selected).mesh.material.wireframe = false;
                     this.selected = null;
+                    this.hideSections(true);
                 }
             }
         });
+    }
+
+    /**
+     * (un)hides the sections that need to be togglable depending on the selected mesh
+     * @param boolean hide or do not hide
+     */
+    hideSections(boolean) {
+        const notice = document.querySelector('#editor p'),
+            editSection = document.querySelector('#editor .content div');
+        if (boolean) {
+            notice.classList.remove('hide');
+            editSection.classList.add('hide');
+        } else {
+            notice.classList.add('hide');
+            editSection.classList.remove('hide');
+        }
     }
 
     /**
@@ -116,8 +137,74 @@ class Configurator {
         this.entities.set(name, new Roof(name));
     }
 
+    /**
+     * Changes the material of the selected mesh
+     * @param material name of the material
+     */
     changeMaterial(material) {
-        
+        const mesh = this.entities.get(this.selected).mesh;
+        mesh.material = this.material.getMaterial(material);
+    }
+
+    /**
+     * Removes the selected mesh
+     */
+    removeMesh() {
+        const mesh = this.entities.get(this.selected).mesh;
+        this.entities.delete(this.selected);
+        mesh.dispose();
+        this.selected = null;
+        this.hideSections(true);
+    }
+
+    /**
+     * Rotates the selected mesh with 90 degrees
+     */
+    rotateMesh() {
+        const mesh = this.entities.get(this.selected).mesh;
+        mesh.rotate(BABYLON.Axis.Y, Utils.degreesToRadians(90), BABYLON.Space.LOCAL);
+    }
+
+    /**
+     * Translates the mesh along an axis
+     * @param axis x, y or z
+     * @param positive boolean
+     */
+    translateMesh(axis, positive) {
+        const mesh = this.entities.get(this.selected).mesh;
+        switch (axis.toUpperCase()) {
+            case 'X':
+                mesh.translate(BABYLON.Axis.X, (positive ? STEP_AMOUNT : -STEP_AMOUNT), BABYLON.Space.WORLD);
+                break;
+            case 'Y':
+                mesh.translate(BABYLON.Axis.Y, (positive ? STEP_AMOUNT : -STEP_AMOUNT), BABYLON.Space.WORLD);
+                break;
+            case 'Z':
+                mesh.translate(BABYLON.Axis.Z, (positive ? STEP_AMOUNT : -STEP_AMOUNT), BABYLON.Space.WORLD);
+                break;
+        }
+    }
+
+    /**
+     * Scales the mesh along an axis
+     * @param axis x, y or z
+     * @param positive boolean
+     */
+    scaleMesh(axis, positive) {
+        const mesh = this.entities.get(this.selected).mesh;
+        const amount = (positive ? STEP_AMOUNT : -STEP_AMOUNT);
+        switch (axis.toUpperCase()) {
+            case 'X':
+                mesh.scaling.x += amount;
+                break;
+            case 'Y':
+                mesh.scaling.y += amount;
+                mesh.position.y += amount;
+                break;
+            case 'Z':
+                mesh.scaling.z += amount;
+                break;
+        }
     }
 
     /**
