7c92925dba20e886385bf85ba5edf40addc5cdd8
Muhammed Sari <muhammed.sari@student.odisee.be>
2017-05-20 21:26:34 +0200

    created settings component

--- src/js/panel/components/Settings.js
+++ src/js/panel/components/Settings.js
@@ -1,3 +1,14 @@
-/**
- * Created by TURK on 20/05/2017.
- */
+import Scene from '../../core/scene';
+
+export default class Settings {
+    constructor() {
+        this.bindEvents();
+    }
+
+    /**
+     * Attaches events to various controls
+     */
+    bindEvents() {
+
+    }
+}
