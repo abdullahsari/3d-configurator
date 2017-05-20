d6385e381e65b5c444cc3856c439fcb1068e3476
Muhammed Sari <muhammed.sari@student.odisee.be>
2017-05-20 22:08:07 +0200

    mute and axes are now able to be set

--- src/js/panel/components/Settings.js
+++ src/js/panel/components/Settings.js
@@ -1,4 +1,5 @@
 import Scene from '../../core/scene';
+import Config from '../../Configurator';
 
 export default class Settings {
     constructor() {
@@ -10,5 +11,23 @@ export default class Settings {
      */
     bindEvents() {
 
+        // Mute setting
+        document.getElementById('mute').addEventListener('change', e => {this.mute(e.target.checked)});
+
+        // Axes setting
+        document.getElementById('axes').addEventListener('change', e => {this.showAxes(e.target.checked)});
+    }
+
+    mute(boolean) {
+        Config.settings.mute = boolean;
+    }
+
+    showAxes(boolean) {
+        if (boolean) {
+            Config.createAxes();
+        } else {
+            for (let axis of Config.axes)
+                axis.dispose();
+        }
     }
 }
